3a4c751f6452f1bfe913df1b7b961c4a8866f3dc
Laura Zhukas <Entertech\lzhukas@a-19hbal4w2vsay.corp.entertechsystems.com>
2019-02-07 09:53:46 -0500

    Added js component

--- app/js/components/button.js
+++ app/js/components/button.js
@@ -6,11 +6,26 @@ const Button= {
   components: {},
   data() {
     return {
+      buttonName: 'v-on:calculate-num="calculateNum"',
+      randVal : 2,
+      value: 0
+    }
+  },
+  methods: {
+    calculateNum : function (value) {
+      console.log ('In buttons.js and value is: ' + value),
+      this.value = value + this.randVal
+    },
+    buttonClicked : function (val) {
+      this.$emit('button-clicked', val)
     }
   },
   created() {
   },
   computed: {
+    message() {
+      console.log('this random value: '+ this.value)
+    }
   },
 }
 export default Button
--- app/js/components/calculator.js
+++ app/js/components/calculator.js
@@ -1,4 +1,11 @@
+/* eslint-disable */
 import Buttons from './button.js'
+let x = 0
+let y = 0
+let switchVariable = false
+let operator = null
+let isNotNumber = false
+let runningSum =0
 const Calculator= {
   name: 'Component',
   props: [],
@@ -11,9 +18,67 @@ const Calculator= {
     return {
     }
   },
-  created() {
+  methods : {
+    getResult: function(x,y, operator) {
+      console.log('In the get result function the x val is: '+x + 'the y val is: '+y)
+        switch(operator){
+        case 'add':
+        return parseFloat(x) + parseFloat(y)
+        case 'subtract':
+        return parseFloat(x) - parseFloat(y)
+        case 'multiply':
+        return parseFloat(x)*parseFloat(y)
+        case 'divide':
+        return parseFloat(x)/parseFloat(y)
+        }
+    },
+    clear() {
+      console.log('In computed'),
+      x =0
+      y=0
+      switchVariable = false
+      operator = null
+    },
+    // catchClicked : function (val) {
+    //   alert(`A button was clicked, with value of ${val}`)
+    // },
+    calculateNum: function (value) {
+      console.log('In calculate Num, was passed: ' + value)
+      isNotNumber = false
+      if (value == 'clear') {
+        this.clear()
+        isNotNumber = true
+      }
+      if (value == 'calculate') {
+        x = this.getResult(x,y,operator)
+        console.log(x)
+        operator = null
+        isNotNumber = true
+        y=0
+      }
+      if (value == 'add' || value =='subtract'|| value=='multiply'||value=='divide'){
+        if (switchVariable) {
+          console.log('in the switchVar if and x val is: '+x+' y val is: '+y
+          + ' the operator is '+operator + ' the value is: '+value)
+          runningSum = this.getResult(x,y,operator) //pass opperator and compute
+          x= runningSum
+          y=0
+        }
+        switchVariable = true
+        operator = value
+        isNotNumber = true
+      }
+      if (switchVariable && !isNotNumber) {
+        y += value
+      } else if (!switchVariable && !isNotNumber) {
+        x += value
+      }
+    }
   },
   computed: {
+    welcomeMessage() {
+      return `Hello ${this.firstname} ${this.lastname}!`
+    }
   },
 }
 export default Calculator
